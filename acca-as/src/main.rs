efd82891edec017cdba2dee1174d2cf6e96136b6
Ariel Abreu <facekapow@outlook.com>
2023-03-26 22:57:45 -0400

    [general] Update license headers
    I had missed a few files, and the ones that I did add a header to,
    I had used the wrong year :P

--- acca-as/src/main.rs
+++ acca-as/src/main.rs
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2022 Ariel Abreu
+// Copyright (C) 2023 Ariel Abreu
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
